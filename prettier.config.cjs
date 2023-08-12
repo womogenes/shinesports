82e8a7b1ac178f1ddccdbd3f1ac9e289dd8372f9
William Y. Feng <womogenes2@gmail.com>
2023-08-11 22:26:32 -0700

    Initialize project with Tailwind and Flowbite

---
+++ prettier.config.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true,
+  tabSize: 2,
+  plugins: ['prettier-plugin-svelte', 'prettier-plugin-tailwindcss'],
+};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Alert } from 'flowbite-svelte';
+</script>
+
+<div class="bg-blue-800 px-10 py-40 text-white">
+  <h1 class="text-3xl font-bold">Hello world!</h1>
+</div>
+
+<div class="p-8">
+  <Alert>
+    <b>Info alert!</b>
+    Change a few things up and try submitting again.
+  </Alert>
+</div>
+
+<style lang="postcss">
+</style>
