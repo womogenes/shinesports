d57c9e0630e4081fdeb24ec12c88fb258f5d6a2d
runny24 <felixhanyu@gmail.com>
2023-10-29 22:43:14 -0700

    About Page Revamp

--- src/routes/(pages)/about/+page.svelte
+++ src/routes/(pages)/about/+page.svelte
@@ -1,22 +1,129 @@
 <script>
-  import MD from '$components/MD.svelte';
+  import Squiggle from '$components/Squiggle.svelte';
 </script>
 
-<main class="mt-4 max-w-3xl">
-  <MD>
-    # About
-    
-RateMyTeam is a website that provides two functions: first, a search engine that allows users to find relevant information about a sports team, such as its location, coach contact information, recruitment information, regional and national rankings, and so on. With this, we aim to provide greater transparency and available information for new athletes. The second is a review function that allows users to review teams based on team culture and coaching quality, and for users to view these reviews. Users can also “rate” the team on a scale from 1-5 stars with their review. With this, we aim to promote healthier team cultures / environments and better emotional wellbeing for athletes. \
- 
-Many athletes join a team not knowing anything about team culture or coaches apart from rumors. Some athletes hope to be on a team that can support them in their athletic endeavors including college recruitment. Finding all this information is difficult without actually joining a team. Many athletes end up regretting their decision and wasting time on a team they don’t enjoy being on or on a team that doesn’t satisfy their needs.
-
-First-person accounts are the best way to get to know a team without having to join it. RateMyTeam will allow athletes and their families to see reviews and make informed decisions. They will also have the opportunity to connect to experienced athletes and coaches to get more information to aid them in making a decision. RateMyTeam also has the potential to encourage coaches to prioritize team culture so that they can market their team better on the website.
-
-“Our goal is simple: to empower junior athletes and their parents to make well-informed decisions when entering the world of athletics. We can transform youth sports by promoting safety, well-being, and transparency for all athletes,” says CEO and co-founder Kaitlin Yeoh.
-
-Users will be introduced to a clean and easy to navigate interface while not missing out on any relevant information. Users have the freedom to search for any team they want and see real reviews. Users can also easily find contact information for coaches and athletes to gain a better understanding of a team.
-  </MD>
-</main>
+<div class="mt-4 w-full">
+  <div class="w-full text-center">
+    <div class="bg-blue-900 p-10 w-full">
+      <h1 class="text-5xl m-10 text-white">
+        The Shine Story
+      </h1>
+    </div>
+    <div class="w-full">
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 2000 45"
+          class="absolute -translate-y-[50%]"
+        >
+          <path
+            d="M2000,17.26a2.6,2.6,0,0,1-.4.49C1970.2,16.56,1970.41,5,1941,5s-29.42,12.75-58.84,12.75S1852.73,5,1823.31,5s-29.42,12.75-58.83,12.75S1735.06,5,1705.64,5s-29.41,12.75-58.83,12.75S1617.39,5,1588,5s-29.41,12.75-58.83,12.75S1499.72,5,1470.3,5s-29.41,12.75-58.83,12.75S1382.05,5,1352.64,5s-29.42,12.75-58.83,12.75S1264.39,5,1235,5s-29.42,12.75-58.83,12.75S1146.73,5,1117.32,5s-29.42,12.75-58.83,12.75S1029.07,5,999.66,5s-29.42,12.75-58.84,12.75S911.41,5,882,5s-29.41,12.75-58.83,12.75S793.74,5,764.33,5,734.91,17.75,705.5,17.75,676.08,5,646.67,5s-29.42,12.75-58.84,12.75S558.42,5,529,5s-29.41,12.75-58.83,12.75S440.76,5,411.34,5s-29.41,12.75-58.83,12.75S323.1,5,293.68,5s-29.42,12.75-58.83,12.75S205.43,5,176,5,146.6,17.75,117.19,17.75,87.77,5,58.36,5C29.11,5,28.93,17.61,0,17.74V50H2000Z"
+            transform="translate(0 0)"
+            fill="white"
+          ></path>
+        </svg>
+      </div>
+    </div>
+    <p class="text-xl text-left text-blue-900 font-bold m-10">
+      Our mission is to empower junior athletes to shine in a healthy and supportive environment that fits their individual needs and dreams.
+    </p>
+    <div class="grid grid-rows-7 gap-20 justify-evenly">
+      <div class="w-full grid grid-cols-2 justify-evenly gap-10 bg-sky-800 p-10 text-white">
+        <div class="flex flex-wrap bg-gray-50 content-center justify-center text-black">
+          <div>Insert Image Here</div>
+        </div>
+        <div>
+          <div class="mb-10">
+            <h2 class="text-4xl m-5">
+              About Shine
+            </h2>
+            <Squiggle class="m-auto w-40 stroke-white"/>
+          </div>
+          <p class="text-xl text-left">
+            Shine strives to provide junior athletes and their parents & guardians with the information necessary to make educated and well-informed decisions when choosing a sports team. We make various key decision making factors of this process transparent, such as the team’s culture, coaching style, competition ranking, practice logistics, cost of attendance, and so on. We believe athletes deserve to be on teams that will make them shine, especially during the most formative years of their life.
+          </p>
+        </div>
+      </div>
+      <div class="w-full grid grid-cols-2 justify-evenly gap-10">
+        <div>
+          <div class="m-10">
+            <h2 class="text-4xl m-5 text-green-900">
+              Safe Sport
+            </h2>
+            <Squiggle class="m-auto w-40 stroke-green-900"/>
+          </div>
+          <p class="text-xl text-left">
+            The International Olympic Committee (IOC) defines Safe Sport as an environment where athletes can train and compete in healthy and supportive surroundings; an environment which is respectful, equitable, and free from all forms of harassment and abuse.
+          </p>
+        </div>
+        <div class="flex flex-wrap content-center justify-center">
+          <img class="" src="/logo.png" alt=""/>
+        </div>
+      </div>
+      <div class="bg-blue-50 p-10">
+        <div>
+          <h2 class="m-10">
+            Meet the Team
+          </h2>
+        </div>
+        <div class="w-full grid grid-rows-2 grid-cols-2 justify-evenly gap-10">
+          <div class="w-full flex flex-col gap-10">
+            <div class="flex flex-wrap h-full content-center justify-center">
+              <img class="" src="/kaitlin.jpg" alt=""/>
+            </div>
+            <div class="flex-none justify-center grid grid-rows-2">
+              <h3 class="text-lg text-center">
+                Kaitlin Yeoh (Founder & CEO) 
+              </h3>
+              <p>
+                Insert Bio Here...
+              </p>
+            </div>
+          </div>
+          <div class="w-full flex flex-col gap-10">
+            <div class="flex flex-wrap h-full content-center justify-center">
+              <img class="" src="/bill.jpg" alt=""/>
+            </div>
+            <div class="flex-none justify-center grid grid-rows-2">
+              <h3 class="text-lg text-center">
+                William Feng (CTO)
+              </h3>
+              <p>
+                Web dev, recreational badminton player.
+              </p>
+            </div>
+          </div>
+          <div class="w-full flex flex-col gap-10">
+            <div class="flex flex-wrap content-center justify-center">
+              <img class="" src="/ayden.jpg" alt=""/>
+            </div>
+            <div class="flex-none justify-center grid grid-rows-2">
+              <h3 class="text-lg text-center">
+                Ayden Tan (SVP)
+              </h3>
+              <p>
+                Insert Bio Here...
+              </p>
+            </div>
+          </div>
+          <div class="w-full flex flex-col gap-10">
+            <div class="flex flex-wrap content-center justify-center">
+              <img class="" src="/felix.jpg" alt=""/>
+            </div>
+            <div class="flex-none justify-center grid grid-rows-2">
+              <h3 class="text-lg text-center">
+                Felix Yu (SVP) 
+              </h3>
+              <p>
+                Developer of Among Us in real life and former crew athlete.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 <style>
   
