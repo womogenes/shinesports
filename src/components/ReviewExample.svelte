6ea4f695c3df8ccf07d80fab57a498283bca9c3c
runny24 <felixhanyu@gmail.com>
2023-11-05 19:40:31 -0800

    review example update

--- src/components/ReviewExample.svelte
+++ src/components/ReviewExample.svelte
@@ -1,9 +1,11 @@
 <script>
     export let text = "Insert Review Here"
     export let image = "logo.png"
+
+    let randomInd = Math.random();
 </script>
 
-<div class="grid grid-cols-2 mx-10" style="width: 1000px;">
+<!-- <div class="grid grid-cols-2 mx-10 bg-gray-50 w-full m-10" style="width: 1000px;">
     <div class="left-0 rounded" style="width: 400px; height: 400px; background-image: url({image});" id="image">
     </div>
     <div class="flex justify-end items-center">
@@ -12,7 +14,17 @@
         </div>
         <img class="bottom-0 m-10 rounded-full object-cover" src="https://source.unsplash.com/random/50x50/?portrait" alt="" style="width: 75px; height: 75px;"/>
     </div>
+</div> -->
 
+<div class="w-full gap-10 grid grid-cols-2">
+    <div class="left-0 rounded" style="background-image: url({image});" id="image">
+    </div>
+    <div class="flex justify-end items-center">
+        <div class="relative bubble-bottom-left flex justify-end items-center p-10 rounded-3xl" style="background-color: #ef4444">
+            <p class="text-left text-3xl text-white font-bold leading-relaxed">{text}</p>
+        </div>
+        <img class="bottom-0 m-10 rounded-full object-cover" src="https://source.unsplash.com/random/50x50/?face/{randomInd}" alt="" style="width: 75px; height: 75px;"/>
+    </div>
 </div>
 
 <style>
