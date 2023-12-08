188de60573187cb5a53e5f6dad6fb9530fd23313
runny24 <felixhanyu@gmail.com>
2023-12-08 09:32:43 -0800

    small formatting change

--- src/components/Review.svelte
+++ src/components/Review.svelte
@@ -6,16 +6,16 @@
 
 <div class="w-full">
     <hr>
-    <div class="w-full p-10 flex flex-cols">
+    <div class="w-full p-10 flex flex-row">
         <div class="w-12 text-center">
             <img src="/defaultprofile.png" alt=""/>
             <p>{review.username}</p>
         </div>
-        <div class="flex flex-2 flex-rows p-10">
+        <div class="flex flex-2 flex-col px-10 h-full bg-blue-40">
             <div>
                 <StarRating setRating="{review.star}" staticStars="{true}"/>
             </div>
-            <div>
+            <div class="py-5">
                 <h5 class="font-bold">{review.title}</h5>
                 <p>{review.comment}</p>
             </div>
